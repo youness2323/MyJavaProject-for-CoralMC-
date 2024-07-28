2e9caaee90440c1f4d1a38655d83dc07245bff7d
youness98 <stef2317ilmannifico@gmail.com>
2024-07-28 13:59:53 +0200

    Update TeleportCommand.java

--- src/commands/TeleportCommand.java
+++ src/commands/TeleportCommand.java
@@ -1,5 +1,26 @@
 package commands;
 
+import modeles.Player;
+import modeles.Location;
+import com.example.minecraftsimulator.PlayerManager;
+
 public class TeleportCommand {
 
-}
+    public static void teleport(Player sender, Player target) {
+        if (target != null) {
+            Location targetLocation = target.getLocation();
+            sender.setLocation(targetLocation);
+            System.out.println(sender.getName() + " is teleported to " + target.getName());
+        } else {
+            System.out.println("Player not found.");
+        }
+    }
+
+    public static void main(String[] args) {
+        PlayerManager playerManager = new PlayerManager();
+        Player sender = playerManager.getPlayer("Player1");
+        Player target = playerManager.getPlayer("Player2");
+
+        if (sender != null && target != null) {
+            teleport(sender, target);
+        } else 
