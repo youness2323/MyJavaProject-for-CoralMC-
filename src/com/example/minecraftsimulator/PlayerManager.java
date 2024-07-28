538c78bcaa4461d742de6ccb9550734d42e35e22
youness98 <stef2317ilmannifico@gmail.com>
2024-07-28 14:01:41 +0200

    Update PlayerManager.java

--- src/com/example/minecraftsimulator/PlayerManager.java
+++ src/com/example/minecraftsimulator/PlayerManager.java
@@ -1,5 +1,21 @@
 package com.example.minecraftsimulator;
 
-public class PlayerManager {
+import com.example.minecraftsimulator.models.Player;
+import java.util.HashMap;
+import java.util.Map;
 
+public class PlayerManager {
+    private Map<String, Player> players;
+
+    public PlayerManager() {
+        this.players = new HashMap<>();
+    }
+
+    public Player getPlayer(String name) {
+        return players.get(name);
+    }
+
+    public void addPlayer(Player player) {
+        players.put(player.getName(), player);
+    }
 }
