b1cde9ae64e0ce8c5eebef975dcecccc9f48f6f8
youness98 <stef2317ilmannifico@gmail.com>
2024-07-28 14:03:02 +0200

    Update player.java

--- src/modeles/player.java
+++ src/modeles/player.java
@@ -1,5 +1,23 @@
-package modeles;
+package com.example.minecraftsimulator.models;
 
-public class player {
+public class Player {
+    private String name;
+    private Location location;
 
+    public Player(String name) {
+        this.name = name;
+        this.location = new Location(0, 0, 0);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void teleportTo(Location location) {
+        this.location = location;
+    }
 }
