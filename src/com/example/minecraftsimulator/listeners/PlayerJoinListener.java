272346b7a401a6e86ed9d9d1488fa6b032df193f
youness98 <stef2317ilmannifico@gmail.com>
2024-07-31 14:24:07 +0200

    Update PlayerJoinListener.java

--- src/com/example/minecraftsimulator/listeners/PlayerJoinListener.java
+++ src/com/example/minecraftsimulator/listeners/PlayerJoinListener.java
@@ -1,1 +1,17 @@
+package com.example.minecraftsimulator.listeners;
+
+import com.example.minecraftsimulator.utils.ConfigManager;
+import com.example.minecraftsimulator.utils.MessageUtils;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerJoinListener implements Listener {
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        String joinMessage = ConfigManager.getString("messages.join")
+                .replace("{player}", event.getPlayer().getName());
+        event.getPlayer().sendMessage(MessageUtils.colorize(joinMessage));
+    }
+}
 
