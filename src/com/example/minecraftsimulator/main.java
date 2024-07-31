c720ddd79cd28b0a6ca87da532984a17765d45e9
youness98 <stef2317ilmannifico@gmail.com>
2024-07-31 14:57:26 +0200

    Update main.java

--- src/com/example/minecraftsimulator/main.java
+++ src/com/example/minecraftsimulator/main.java
@@ -19,7 +19,27 @@ public class Main extends JavaPlugin {
         getCommand("fly").setExecutor(new FlyCommand());
         getCommand("teleport").setExecutor(new TeleportCommand());
         getCommand("broadcast").setExecutor(new BroadcastCommand());
-        
+
+        getCommand("tpa").setExecutor(new TpaCommand());
+        getCommand("tpaccept").setExecutor(new TpacceptCommand());
+        getCommand("tpdeny").setExecutor(new TpdenyCommand());
+        getCommand("msg").setExecutor(new MsgCommand());
+        getCommand("reply").setExecutor(new ReplyCommand());
+        getCommand("warpcreate").setExecutor(new WarpCreateCommand());
+        getCommand("warpdelete").setExecutor(new WarpDeleteCommand());
+        getCommand("give").setExecutor(new GiveCommand());
+        getCommand("ban").setExecutor(new BanCommand());
+        getCommand("unban").setExecutor(new UnbanCommand());
+        getCommand("kick").setExecutor(new KickCommand());
+        getCommand("mute").setExecutor(new MuteCommand());
+        getCommand("unmute").setExecutor(new UnmuteCommand());
+        getCommand("time").setExecutor(new TimeCommand());
+        getCommand("weather").setExecutor(new WeatherCommand());
+        getCommand("spawnmob").setExecutor(new SpawnMobCommand());
+        getCommand("gamemode").setExecutor(new GamemodeCommand());
+        getCommand("effect").setExecutor(new EffectCommand());
+        getCommand("enchant").setExecutor(new EnchantCommand());
+
         // Register event listeners
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerQuitListener(), this);
