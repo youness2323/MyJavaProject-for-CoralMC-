03fe6b142bd7066f6644457a7e24530367577947
youness98 <stef2317ilmannifico@gmail.com>
2024-07-31 14:28:48 +0200

    Update ConfigManager.java

--- src/com/example/minecraftsimulator/utils/ConfigManager.java
+++ src/com/example/minecraftsimulator/utils/ConfigManager.java
@@ -1,1 +1,29 @@
+package com.example.minecraftsimulator.utils;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ConfigManager {
+    private static FileConfiguration config;
+
+    public static void init(JavaPlugin plugin) {
+        config = plugin.getConfig();
+    }
+
+    public static FileConfiguration getConfig() {
+        return config;
+    }
+
+    public static String getString(String path) {
+        return config.getString(path);
+    }
+
+    public static int getInt(String path) {
+        return config.getInt(path);
+    }
+
+    public static boolean getBoolean(String path) {
+        return config.getBoolean(path);
+    }
+}
 
