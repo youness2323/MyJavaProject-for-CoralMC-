af45e5e575b2428db611f709744e6a178045f89e
youness98 <stef2317ilmannifico@gmail.com>
2024-07-31 13:09:44 +0200

    Update main.java

--- src/com/example/minecraftsimulator/main.java
+++ src/com/example/minecraftsimulator/main.java
@@ -1,33 +1,35 @@
-package com.example.ultimateminecraftplugin;
+package com.myplugin;
 
+import com.myplugin.commands.*;
+import com.myplugin.listeners.*;
+import com.myplugin.utils.ConfigManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import com.example.ultimateminecraftplugin.commands.*;
-import com.example.ultimateminecraftplugin.listeners.PlayerJoinListener;
 
 public class Main extends JavaPlugin {
-
     @Override
     public void onEnable() {
-        getLogger().info("UltimateMinecraftPlugin abilitato!");
+        // Save default config if it doesn't exist
+        saveDefaultConfig();
+        
+        // Initialize config manager
+        ConfigManager.init(this);
 
-        // Registrazione dei comandi
-        getCommand("tpa").setExecutor(new TeleportCommand(this));
-        getCommand("tpaccept").setExecutor(new TeleportCommand(this));
-        getCommand("tp").setExecutor(new TeleportCommand(this));
-        getCommand("balance").setExecutor(new EconomyCommand(this));
-        getCommand("pay").setExecutor(new EconomyCommand(this));
-        getCommand("claim").setExecutor(new ProtectionCommand(this));
-        getCommand("unclaim").setExecutor(new ProtectionCommand(this));
-        getCommand("minigame").setExecutor(new MinigameCommand(this));
-        getCommand("leaderboard").setExecutor(new LeaderboardCommand(this));
-        getCommand("customizeworld").setExecutor(new WorldCustomizationCommand(this));
+        // Register commands
+        getCommand("heal").setExecutor(new HealCommand());
+        getCommand("fly").setExecutor(new FlyCommand());
+        getCommand("teleport").setExecutor(new TeleportCommand());
+        getCommand("broadcast").setExecutor(new BroadcastCommand());
+        
+        // Register event listeners
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerQuitListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerDeathListener(), this);
 
-        // Registrazione degli eventi
-        getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
+        getLogger().info("Plugin enabled!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("UltimateMinecraftPlugin disabilitato!");
+        getLogger().info("Plugin disabled!");
     }
 }
