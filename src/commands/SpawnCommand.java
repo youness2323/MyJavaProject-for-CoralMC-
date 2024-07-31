a1b790ae98141cf00ed01b06d7ed891f24d63ce3
youness98 <stef2317ilmannifico@gmail.com>
2024-07-31 14:46:24 +0200

    Create SpawnCommand.java

---
+++ src/commands/SpawnCommand.java
@@ -0,0 +1,23 @@
+package com.example.minecraftsimulator.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SpawnCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Questo comando può essere usato solo dai giocatori.");
+            return false;
+        }
+
+        Player player = (Player) sender;
+        player.teleport(player.getWorld().getSpawnLocation());
+        player.sendMessage("Teletrasportato al punto di spawn.");
+
+        return true;
+    }
+}
